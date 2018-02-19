704fc215077d0349277f2bf44f77fb1c7df1fbab
Niels Van Walleghem <niels.van.walleghem@student.howest.be>
2018-02-19 11:29:05 +0100

    bhhdbdh

---
+++ DotNetCoreMVCVoorbeeld/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreMVCVoorbeeld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string name)
+        {
+            return $"Hallo{name}";
+        }
+    }
+}
